371136b85f1ba3d2d73af26a0a493780f2e1478c
Laksito Pamilih <laksito@pm.me>
2021-06-06 12:29:48 +0700

    create Hat class

---
+++ main.py
@@ -0,0 +1,10 @@
+import prob_calculator
+
+hat = prob_calculator.Hat(blue=4, red=2, green=6)
+# probability = prob_calculator.experiment(
+#     hat=hat,
+#     expected_balls={"blue": 2,
+#                     "red": 1},
+#     num_balls_drawn=4,
+#     num_experiments=3000)
+# print("Probability:", probability)
