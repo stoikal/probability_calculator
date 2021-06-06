e4b1b4ca9f91e088901496b6a11380b5015e19fd
Laksito Pamilih <laksito@pm.me>
2021-06-06 14:20:31 +0700

    fix experiment method

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,7 +3,46 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    def __init__(self, red, green, blue):
-        print(red)
+    def __init__(self, **colors):
+        self.contents = []
 
-# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+        for key, val in colors.items():
+            for i in range(val):
+                self.contents.append(key)
+
+    def draw(self, num_balls_drawn):
+        result = []
+        if num_balls_drawn >= len(self.contents):
+            result = self.contents
+            self.contents = []
+            return result
+        
+        for i in range(num_balls_drawn):
+            index_to_pop = random.randint(0, len(self.contents) - 1)
+            result.append(self.contents.pop(index_to_pop))
+
+        return result
+
+def __is_expected(result, expected):
+    is_expected = True
+    for color, expected_value in expected.items():
+        if result.count(color) < expected_value:
+            return False
+
+    return is_expected
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    N = num_experiments
+    M = 0
+
+    for i in range(N):
+        hat_copy = copy.deepcopy(hat)
+        drawn = hat_copy.draw(num_balls_drawn)
+        
+        if __is_expected(drawn, expected_balls):
+            M += 1
+
+    if M: 
+        return M / N
+    else:
+        return M
